cd59969d690ee749dd0273d285ba839803d495eb
Bryan13133 <52942708+Bryan13133@users.noreply.github.com>
2020-07-21 23:42:56 -0600

    First Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,48 @@
+import { NotFoundComponent } from './Components/NotFoundComponent/not-found/not-found.component';
+import { StoreComponent } from './Components/StoreComponent/store/store.component';
+import { AccountComponent } from './Components/AccountComponent/account/account.component';
+import { SubscribeComponent } from './Components/SubscribeComponent/subscribe/subscribe.component';
+import { HomeComponent } from './Components/HomeComponent/home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes=[
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'subscribe',
+    component: SubscribeComponent
+  },
+  {
+    path: 'account',
+    component: AccountComponent
+  },
+  {
+    path: 'store',
+    component: StoreComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: 
+  [
+    RouterModule.forRoot(routes)
+  ],
+  exports:
+  [
+    RouterModule
+  ]
+
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
+import { AccountComponent } from './Components/AccountComponent/account/account.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/HomeComponent/home/home.component';
+import { SubscribeComponent } from './Components/SubscribeComponent/subscribe/subscribe.component';
+import { StoreComponent } from './Components/StoreComponent/store/store.component';
+import { NotFoundComponent } from './Components/NotFoundComponent/not-found/not-found.component';
+import { ToolbarComponent } from './Components/ToolBarComponent/toolbar/toolbar.component';
+import { FooterComponent } from './Components/FooterComponent/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SubscribeComponent,
+    AccountComponent,
+    StoreComponent,
+    NotFoundComponent,
+    ToolbarComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
